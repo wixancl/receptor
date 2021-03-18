eba34cb213e7c19a88da4fc5ae83002d0a9490ac
guido.rios <guido.rios@redsalud.gov.cl>
2021-03-18 11:21:31 -0300

    2021_03_18_11_21_31

--- admin/configuracion.php
+++ admin/configuracion.php
@@ -61,7 +61,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title" id="exampleModalLongTitle">Nueva encuesta</h5>
+        <h4 class="modal-title" id="exampleModalLongTitle">llave</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -71,37 +71,16 @@
               <div class="modal-body">
                   
                         <div class="form-group">
-                          <label for="txtnombre" class="col-sm-4 col-form-label">Nombre de la encuesta</label>
+                          <label for="txtnombre" class="col-sm-4 col-form-label">llave</label>
                           <div class="col-sm-8">
-                              <input type="text" id="txtnombre" name="txtnombre" style="width:100%">
+                              <input type="text" id="txtnombre" name="txtnombre" style="width:50%">
                           </div>
+
+
+
                         </div>
-                        <br>
-                        <hr>
-                        <h4> Preguntas</h4>
-                        <hr>
-                        <br>
-                        <table id="camposdinamicos">
-                          <tr>  
-                              <td>
-                                 <label for="txtnombre" class="col-form-label" style="margin-right:5px">Pregunta 1</label>
-                              </td>
-                              <td>
-                                  <input type="text" name="name[]" id="name" class="form-control name_list">
-                              </td>
-                              <td>
-                                <select name="type[]" id="type" class="form-control type_list"  style="margin-left:5px">
-                                      <option value="1" select>SI - NO</option>
-                                      <option value="2"> Rango 0 - 5</option>
-                                      <option value="3"> Respuesta breve</option>
-                                </select>
-                              
-                              </td>
-                              <td>
-                                  <button name="add" id="add" class="btn btn-success" style="margin-left:15px">Agregar mas</button>
-                              </td>
-                          </tr>
-                        </table>
+          
+   
 
 
               </div>
